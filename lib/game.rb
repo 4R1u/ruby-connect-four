44771baaa00d9f405c27dfa26a7ca146cb1664a9
4R1u <62248461+4R1u@users.noreply.github.com>
2025-01-02 12:01:36 +0400

    feat: Game#insert_disc

---
+++ lib/game.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# represents the toy in which the discs go
+class Game
+  def initialize
+    @grid = Array.new(6) { Array.new(7) }
+  end
+
+  def insert_disc(player, column)
+    (0..5).reverse_each do |row|
+      if @grid[row][column].nil?
+        @grid[row][column] = player
+        return row
+      end
+    end
+    nil
+  end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative '../lib/game'
+
+describe Game do
+  subject(:game) { described_class.new }
+  describe '#insert_disc' do
+    context 'when the column is empty' do
+      it 'returns 5' do
+        expect(game.insert_disc('r', 0)).to eq(5)
+      end
+    end
+
+    context 'when the column has one disc' do
+      before do
+        game.insert_disc('r', 0)
+      end
+      it 'returns 4' do
+        expect(game.insert_disc('y', 0)).to eq(4)
+      end
+    end
+
+    context 'when the column is full' do
+      before do
+        game.insert_disc('r', 0)
+        game.insert_disc('y', 0)
+        game.insert_disc('r', 0)
+        game.insert_disc('y', 0)
+        game.insert_disc('r', 0)
+        game.insert_disc('y', 0)
+      end
+
+      it 'returns nil' do
+        expect(game.insert_disc('r', 0)).to be_nil
+      end
+    end
+  end
+end
