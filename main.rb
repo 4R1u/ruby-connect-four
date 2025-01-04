cb6e76f68b168bddf6780f59029bb44f124eb9be
4R1u <62248461+4R1u@users.noreply.github.com>
2025-01-04 22:13:27 +0400

    fix: display right range when asking for col input

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ loop do
   current_player = 'r'
   until game.winner?(players.difference([current_player])[0]) || game.full?
     game.display_grid
-    puts "current player: #{current_player}.\nWhich column would you like to insert a disc in? (1..7)"
+    puts "current player: #{current_player}.\nWhich column would you like to insert a disc in? (0..6)"
     inp = 8
     inp = gets.to_i until game.insert_disc(current_player, inp)
     current_player = players.difference([current_player])[0]
