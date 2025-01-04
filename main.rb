fa322644867ce288ce3b6766224544c2ab05fe47
4R1u <62248461+4R1u@users.noreply.github.com>
2025-01-04 21:50:00 +0400

    feat: gameloop

---
+++ main.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'lib/game'
+
+loop do
+  game = Game.new
+  players = %w[r y]
+  current_player = 'r'
+  until game.winner?(players.difference([current_player])[0]) || game.full?
+    p game.grid
+    puts "current player: #{current_player}.\nWhich column would you like to insert a disc in? (1..7)"
+    inp = 8
+    inp = gets.to_i until game.insert_disc(current_player, inp)
+    current_player = players.difference([current_player])[0]
+    puts 'would you like to quit? [y]'
+    break if gets.chomp == 'y'
+  end
+end
